fd41f11068a608dd5a463d31f0c36e2588f0c97b
Bohdan Solomchak <bohdan.jur@gmail.com>
2023-06-23 12:06:00 +0200

    lesson13StartDallTypeStart

--- src/Components/todoApi/todoListApi.ts
+++ src/Components/todoApi/todoListApi.ts
@@ -1,21 +1,30 @@
 import axios from "axios";
 
-const settings = {
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true
-}
+})
+
+
 export const todoListApi = {
     getTodoLists() {
-        return axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
-
+        return instance.get<TodoListDallType[]>('todo-lists')
     },
     postTodoLists(title: string) {
-        return axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title}, settings)
+        return instance.post('todo-lists', {title})
     },
     delTodoLists(todoID: string) {
-        return axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todoID}`, settings)
+        return instance.delete(`todo-lists/${todoID}`)
     },
     updateTodoLists(todoID: string, title: string) {
-        return axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todoID}`, {title}, settings)
+        return instance.put(`todo-lists/${todoID}`, {title})
     }
 
+}
+
+type TodoListDallType = {
+    id: string,
+    title: string,
+    addedDate: Date,
+    order: number
 }
